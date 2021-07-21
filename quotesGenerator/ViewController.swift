4cc5603b32b4e9153d0ec5bb074fcecf04291960
Murat Menzilci <menzilcim>
2021-07-21 10:32:07 +0300

    Button and label added

--- quotesGenerator/ViewController.swift
+++ quotesGenerator/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    
+    let arrayOfQuotes = ["It's a wonderfull day today!", "You are perfect!", "Be yourself because you are amazing!", "Your awesome!", "Nothing can stop you", "It's a great day"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        let randomNumber = Int.random(in: 0...(arrayOfQuotes.count-1))
+        myLabel.text = arrayOfQuotes[randomNumber]
+    }
+    
 }
 
